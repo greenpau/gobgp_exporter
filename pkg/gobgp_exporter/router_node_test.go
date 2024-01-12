af88179b8c6818c28662bf826c7de6281f61d4f2
Francesco Berni <kurojishi@kurojishi.me>
2024-01-12 12:47:53 +0100

    More complete validation of endpoint for gobgp grpc

---
+++ pkg/gobgp_exporter/router_node_test.go
@@ -0,0 +1,59 @@
+// Copyright 2018 Paul Greenberg (greenpau@outlook.com)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package exporter
+
+import (
+	"testing"
+
+	"github.com/prometheus/common/promlog"
+)
+
+func TestValidAddress(t *testing.T) {
+	allowedLogLevel := &promlog.AllowedLevel{}
+	if err := allowedLogLevel.Set("debug"); err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	promlogConfig := &promlog.Config{
+		Level: allowedLogLevel,
+	}
+
+	logger := promlog.New(promlogConfig)
+
+	cases := []struct {
+		address string
+		ok      bool
+	}{
+		{address: "127.0.0.1:50051", ok: true},
+		{address: "", ok: false},
+		{address: "127.0.0.1:500511", ok: false},
+		{address: "localaddress:50051", ok: true},
+        {address: "https://localaddress:50051", ok: true},
+		{address: "http://localaddress:50051", ok: true},
+		{address: "fuuuu://localaddress:50051", ok: false},
+		{address: "dns:///localhost:50051", ok: false},
+		{address: "[::1]:50051", ok: true},
+		{address: "::1:50051", ok: false},
+	}
+	for _, test := range cases {
+		err := validAddress(test.address, logger)
+		if test.ok && err != nil {
+			t.Errorf("expected no error w/ %q, but got %q", test.address, err)
+		}
+		if !test.ok && err == nil {
+			t.Errorf("expected error w/ %q, but got %q", test.address, err)
+		}
+	}
+}
